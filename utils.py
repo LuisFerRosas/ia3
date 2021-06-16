8cdfdc2e2f06b700f63cc06b04889fd495a6b08f
Luis Fernando Duran <luis3658duran@gmail.com>
2021-06-16 18:48:40 -0400

    device

--- utils.py
+++ utils.py
@@ -138,16 +138,17 @@ def guided_attention(N, T, g=0.2):
 
 
 def generate_square_subsequent_mask(sz):
-    mask = (torch.triu(torch.ones((sz, sz))) == 1).transpose(0, 1)
+    DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    mask = (torch.triu(torch.ones((sz, sz),device=DEVICE)) == 1).transpose(0, 1)
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
     return mask
 
 def create_mask(src, tgt,PAD_IDX):
   src_seq_len = src.shape[0]
   tgt_seq_len = tgt.shape[0]
-
+  DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
   tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
-  src_mask = torch.zeros((src_seq_len, src_seq_len)).type(torch.bool)
+  src_mask = torch.zeros((src_seq_len, src_seq_len),device=DEVICE).type(torch.bool)
 
   src_padding_mask = (src == PAD_IDX).transpose(0, 1)
   tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1)
